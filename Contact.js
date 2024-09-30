8f06d3ffa8264aeea5d5bffb5f29a647c664eeb0
Hassan3322751 <141483001+Hassan3322751@users.noreply.github.com>
2024-09-30 06:56:04 -0700

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -7,9 +7,8 @@ const router = express.Router();
 router.get("/contact", async(req, res) => {
     // const { name, email, website, message } = JSON.parse(req.body);
     const name = "Hassan";
-    const email = "hassanraza155156@gmail.com";
     const message = "message";
-    const website = "sumizan";
+    const website = "Chat-Web"; //Your web name
 
    
        let transport= NodeMailer.createTransport({
@@ -23,13 +22,13 @@ router.get("/contact", async(req, res) => {
             }
         })
         let mailOptions={
-            from: 'Sumizan',
-            to:"mianhassan155156@gmail.com",
-            subject:`SUMIZAN - Message from ${name} and the website is ${website!=""?website:"no site"} `,
+            from: 'Your Web',
+            to: "example@gmail.com",  //Email whose you want to send mail
+            subject:` - Message from ${name} and the website is ${website!=""?website:"no site"} `,
             text:message+"                               sender email="+email
         }
         await transport.sendMail(mailOptions).then(()=>{
-        console.log("yes worked")
+        console.log("Email Sent Successfully")
         // return res.status(200).json({message:"Success"})
        }).catch((err)=>{
         console.log('error')
