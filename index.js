d534340393e6701aa9252a82194d65a4e226adc5
Hassan3322751 <141483001+Hassan3322751@users.noreply.github.com>
2024-09-30 06:57:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ import contact from './Contact.js'
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(bodyParser.json());
+app.use(bodyParser.json());  //Middleware to parse body data from Frontend
 app.use(express.json());
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use( contact )
